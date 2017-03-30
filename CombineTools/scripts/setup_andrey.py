49b6f29cb2c1cbfedba075b5c075af60834b079e
mauro verzetti <mverzett@cern.ch>
2017-03-30 03:44:43 -0500

    refactored code

--- CombineTools/scripts/setup_andrey.py
+++ CombineTools/scripts/setup_andrey.py
@@ -2,6 +2,12 @@
 
 import CombineHarvester.CombineTools.ch as ch
 import os
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('inputfile')
+parser.add_argument('limit', choices=['electrons', 'muons', 'cmb'], help='choose leptonic decay type')
+args = parser.parse_args()
 
 cb = ch.CombineHarvester()
 
@@ -11,123 +17,122 @@ aux_shapes   = auxiliaries
 addBBB = False
 
 masses = ['400', '500', '600', '750']
+widths = ['5', '10', '25', '50']
 
 # for mode in ['scalar']:#, 'pseudoscalar']:
-for mode in ['pseudoscalar']:#, 'pseudoscalar']:
+for parity in ['pseudoscalar']:#, 'pseudoscalar']:
+	for width in widths:
+		mode='{mode}_{width}pc'.format(mode=parity, width=width)
+		patterns = [i % width for i in ['ggA_pos-int-%spc-M', 'ggA_neg-int-%spc-M', 'ggA_pos-sgn-%spc-M']]
+		procs = {
+			# 'sig'  : ['S0_{mode}_M'.format(mode=mode), 'S0_neg_{mode}_M'.format(mode=mode)],
+			'sig'   : patterns,
+			# 'sim'  : ['WZ', 'ZZ', 'ttW', 'ttZ', 'ttH'],
+			'bkg'  : ['TT', 'VV', 'TTV',	'tChannel', 'tWChannel', 'WJets', 'ZJets']
+		}
+		if args.limit == 'electrons' or args.limit == 'cmb':
+			procs['bkg'].append('QCDejets')
+		if args.limit == 'muons' or args.limit == 'cmb':
+			procs['bkg'].append('QCDmujets')
+	
+		cats = []
+		if args.limit == 'muons' or args.limit == 'cmb':
+			cats.append((len(cats), 'mujets'))
+		if args.limit == 'electrons' or args.limit == 'cmb':
+			cats.append((len(cats), 'ejets'))
 
-  patterns = ['HToTT-sgn-{mode}-m', 'HToTT-int-{mode}-m', 'HToTT-int-{mode}_neg-m']
-  procs = {
-    # 'sig'  : ['S0_{mode}_M'.format(mode=mode), 'S0_neg_{mode}_M'.format(mode=mode)],
-    'sig'   : [pattern.format(mode=mode) for pattern in patterns],
-    # 'sim'  : ['WZ', 'ZZ', 'ttW', 'ttZ', 'ttH'],
-    'bkg'  : ['Wjets', 'single-top', 'VV', 'DY', 'ttbar', 'QCD']
-  }
+		cb.AddObservations(['*'], ['mauro'], ["13TeV"], ['htt'], cats)
+		cb.AddProcesses(['*'], ['mauro'], ["13TeV"], ['htt'], procs['bkg'], cats, False)
+		cb.AddProcesses(masses, ['mauro'], ["13TeV"], ['htt'], procs['sig'], cats, True)
+		# cb.AddProcesses(['*'], ['mauro'], ["13TeV"], ['htt'], procs['sig'], cats, True)
 
-  cats = [(0, '1b'), (1, '2b')]
+		print '>> Adding systematic uncertainties...'
+		#
+		# Normalization uncertainty
+		#
+		cb.cp().process(procs['sig'] + procs['bkg']).AddSyst(
+				cb, 'lumi_13TeV', 'lnN', ch.SystMap()(1.026))
+	
+		cb.cp().process(['VV']).AddSyst(
+				cb, 'CMS_httbar_vvXsec_13TeV', 'lnN', ch.SystMap()(1.3))
+	
+		cb.cp().process(['TT']).AddSyst(
+				cb, 'ttbar_rate', 'lnN', ch.SystMap()(1.05))
+	
+		cb.cp().process(['tChannel']).AddSyst(
+				cb, 'st_rate', 'lnN', ch.SystMap()(1.05))
 
-  
+		cb.cp().process(['tWChannel']).AddSyst(
+				cb, 'st_rate', 'lnN', ch.SystMap()(1.05))
+	
+		cb.cp().process(['WJets']).AddSyst(
+				cb, 'w_rate', 'lnN', ch.SystMap()(1.03))
+		cb.cp().process(['ZJets']).AddSyst(
+				cb, 'w_rate', 'lnN', ch.SystMap()(1.03))
+		cb.cp().process(procs['sig'] + procs['bkg']).AddSyst(
+				cb, 'trigger', 'lnN', ch.SystMap()(1.0115))
 
-  cb.AddObservations(['*'], ['andrey'], ["8TeV"], ['htt'], cats)
-  cb.AddProcesses(['*'], ['andrey'], ["8TeV"], ['htt'], procs['bkg'], cats, False)
-  cb.AddProcesses(masses, ['andrey'], ["8TeV"], ['htt'], procs['sig'], cats, True)
-  # cb.AddProcesses(['*'], ['andrey'], ["8TeV"], ['htt'], procs['sig'], cats, True)
+		#
+		# Shape uncetainties
+		#
+		#on everything
+		for name in [
+			'CMS_scale_j_13TeV',
+			'CMS_eff_b_13TeV',
+			'CMS_fake_b_13TeV',
+			'CMS_pileup',
+			'CMS_METunclustered_13TeV'
+			]:
+			cb.cp().process(procs['sig'] + procs['bkg']).AddSyst(
+				cb, name, 'shape', ch.SystMap()(1.))
 
-  print '>> Adding systematic uncertainties...'
+		#on TT only
+		for name in [
+			'QCDscaleMERenorm_TT',
+			'QCDscaleMEFactor_TT',
+			'QCDscaleMERenormFactor_TT',
+			'QCDscalePS_TT',
+			'TMass',
+			'pdf'
+			]:
+			cb.cp().process(['TT']).AddSyst(
+				cb, name, 'shape', ch.SystMap()(1.))
 
-  cb.cp().process(procs['sig'] + procs['bkg']).AddSyst(
-      cb, 'lumi', 'lnN', ch.SystMap()(1.026))
+		if args.limit == 'muons' or args.limit == 'cmb':
+			cb.cp().process(['QCDmujets']).AddSyst(
+				cb, 'QCD_mu_norm', 'lnN', ch.SystMap()(2.))
+		if args.limit == 'electrons' or args.limit == 'cmb':
+			cb.cp().process(['QCDejets']).AddSyst(
+				cb, 'QCD_el_norm', 'lnN', ch.SystMap()(2.))
+			
 
-  cb.cp().process(['VV']).AddSyst(
-      cb, 'diboson_rate', 'lnN', ch.SystMap()(1.3))
+		print '>> Extracting histograms from input root files...'
+		in_file = args.inputfile
+		cb.cp().backgrounds().ExtractShapes(
+				in_file, '$BIN/$PROCESS', '$BIN/$PROCESS_$SYSTEMATIC')
+		cb.cp().signals().ExtractShapes(
+				in_file, '$BIN/$PROCESS$MASS', '$BIN/$PROCESS$MASS_$SYSTEMATIC')
+			# in_file, '$BIN/$PROCESS', '$BIN/$PROCESS__$SYSTEMATIC')
 
-  cb.cp().process(['ttbar']).AddSyst(
-      cb, 'ttbar_rate', 'lnN', ch.SystMap()(1.05))
+		if addBBB:
+			bbb = ch.BinByBinFactory().SetAddThreshold(0.).SetFixNorm(False)
+			bbb.MergeBinErrors(cb.cp().backgrounds())
+			bbb.AddBinByBin(cb.cp().backgrounds(), cb)
 
-  cb.cp().process(['single-top']).AddSyst(
-      cb, 'st_rate', 'lnN', ch.SystMap()(1.05))
+		print '>> Setting standardised bin names...'
+		ch.SetStandardBinNames(cb)
+		cb.PrintAll()
 
-  cb.cp().process(['Wjets']).AddSyst(
-      cb, 'w_rate', 'lnN', ch.SystMap()(1.03))
-
-  cb.cp().process(['DY']).AddSyst(
-      cb, 'dy_rate', 'lnN', ch.SystMap()(1.03))
-
-  cb.cp().process(procs['sig'] + procs['bkg']).AddSyst(
-      cb, 'trigger', 'lnN', ch.SystMap()(1.0115))
-
-  cb.cp().process(['ttbar']).AddSyst(
-      cb, 'scaleTT', 'shape', ch.SystMap()(1.))
-
-  cb.cp().process(procs['sig'] + procs['bkg']).AddSyst(
-      cb, 'bTag', 'shape', ch.SystMap()(1.))
-
-  cb.cp().process(procs['sig'] + procs['bkg']).AddSyst(
-      cb, 'jec', 'shape', ch.SystMap()(1.))
-
-  cb.cp().process(procs['sig'] + procs['bkg']).AddSyst(
-      cb, 'jer', 'shape', ch.SystMap()(1.))
-
-  cb.cp().process(procs['sig'] + procs['bkg']).AddSyst(
-      cb, 'pu', 'shape', ch.SystMap()(1.))
-
-  cb.cp().process(procs['sig'] + procs['bkg']).AddSyst(
-      cb, 'muID', 'shape', ch.SystMap()(1.))
-
-  cb.cp().process(procs['sig'] + procs['bkg']).AddSyst(
-      cb, 'eID', 'shape', ch.SystMap()(1.))
-
-  cb.cp().process(procs['sig'] + procs['bkg']).AddSyst(
-      cb, 'metUncl', 'shape', ch.SystMap()(1.))
-
-  cb.cp().process(procs['sig'] + procs['bkg']).AddSyst(
-      cb, 'bMistag', 'shape', ch.SystMap()(1.))
-
-  cb.cp().process(['Wjets']).AddSyst(
-      cb, 'scaleW', 'shape', ch.SystMap()(1.))
-
-  cb.cp().process(['DY']).AddSyst(
-      cb, 'scaleDY', 'shape', ch.SystMap()(1.))
-  
-
-  print '>> Extracting histograms from input root files...'
-  in_file = aux_shapes + 'andrey_tth.inputs-bsm-8TeV.root'
-  cb.cp().backgrounds().ExtractShapes(
-      in_file, '$BIN/$PROCESS', '$BIN/$PROCESS__$SYSTEMATIC')
-  cb.cp().signals().ExtractShapes(
-      in_file, '$BIN/$PROCESS$MASS', '$BIN/$PROCESS$MASS__$SYSTEMATIC')
-    # in_file, '$BIN/$PROCESS', '$BIN/$PROCESS__$SYSTEMATIC')
-
-
-  # print '>> Generating bbb uncertainties...'
-  # bbb = ch.BinByBinFactory()
-  # bbb.SetAddThreshold(0.1).SetFixNorm(True)
-  # bbb.AddBinByBin(cb.cp().process(['reducible']), cb)
-
-
-  # for mass in masses:
-  #   norm_initial = norms[mode][int(mass)] 
-
-  #   cb.cp().process(['S0_neg_{mode}_M{mass}'.format(mode=mode, mass=mass), 'S0_{mode}_M{mass}'.format(mode=mode, mass=mass)]).ForEachProc(lambda p: p.set_rate(p.rate()/norm_initial))
-
-  if addBBB:
-    bbb = ch.BinByBinFactory().SetAddThreshold(0.).SetFixNorm(False)
-    bbb.MergeBinErrors(cb.cp().backgrounds())
-    bbb.AddBinByBin(cb.cp().backgrounds(), cb)
-
-  print '>> Setting standardised bin names...'
-  ch.SetStandardBinNames(cb)
-  cb.PrintAll()
-
-  writer = ch.CardWriter('$TAG/$MASS/$ANALYSIS_$CHANNEL_$BINID.txt',
-  # writer = ch.CardWriter('$TAG/$ANALYSIS_$CHANNEL_$BINID_$ERA.txt',
-                         '$TAG/$ANALYSIS_$CHANNEL.input.root')
-  # writer.SetVerbosity(100)
-  writer.WriteCards('output/{mode}'.format(mode=mode), cb)
-  print 'Try writing cards...'
-  # import ROOT
-  # f_out = ROOT.TFile('andrey_out.root', 'RECREATE')
-  # cb.WriteDatacard("andrey_out.txt", 'andrey_out.root')
-  # writer.WriteCards('output/andrey_cards/', cb)
+		writer = ch.CardWriter(
+			'$TAG/$MASS/$ANALYSIS_$CHANNEL_$BINID.txt',
+			'$TAG/$ANALYSIS_$CHANNEL.input.root')
+		# writer.SetVerbosity(100)
+		writer.WriteCards('output/{mode}'.format(mode=mode), cb)
+		print 'Try writing cards...'
+		# import ROOT
+		# f_out = ROOT.TFile('andrey_out.root', 'RECREATE')
+		# cb.WriteDatacard("andrey_out.txt", 'andrey_out.root')
+		# writer.WriteCards('output/andrey_cards/', cb)
 
 print '>> Done!'
 
