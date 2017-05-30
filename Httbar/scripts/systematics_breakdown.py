5cdabc13c27e73f8eb2dff8dce56733f5cc7b028
mauro verzetti <mverzett@cern.ch>
2017-05-30 11:43:17 -0500

    End of the day commit

--- Httbar/scripts/systematics_breakdown.py
+++ Httbar/scripts/systematics_breakdown.py
@@ -11,30 +11,36 @@ from pdb import set_trace
 def call(cmd):
 	proc = Popen(cmd, stdout=PIPE, stderr=STDOUT, shell=True)
 	stdout, nothing = proc.communicate()    
-	return proc.wait()
+	code = proc.wait()
+	if code != 0:
+		raise RuntimeError(
+			'\n\nCommand: %s \n\nFailed with code %d and error \n\n %s' \
+				% (cmd, code, nothing)
+			)
+	return code
 
 def backup(src):
 	ret = '%s.bak' % src
-	shutil.copyfile(src, ret)
+	shutil.move(src, ret)
 	return ret
 
 def unbackup(src):
 	if not src.endswith('.bak'):
 		raise ValueError('%s is not a backup file!')
-	shutil.copyfile(src, src[:-4])
+	shutil.move(src, src[:-4])
 	return src[:-4]
 
 def tag(src, value):
 	dname, bname = os.path.dirname(src), os.path.basename(src)
 	ret = '%s/%s_%s' % (dname, value, bname)
-	shutil.copyfile(src, ret)
+	shutil.move(src, ret)
 	return ret
 
 parser = ArgumentParser()
 parser.add_argument('--run', choices=['blind'])
 args = parser.parse_args()
 
-txts = glob('[AH]_[0-9]*/000/combined.txt.cmb')# % args.jobid)
+txts = glob('[AH]_[0-9]*/[0-9][0-9][0-9]/combined.txt.cmb')# % args.jobid)
 if not txts:
 	raise RuntimeError('I could not find any datacard available')
 txt = txts[0]
@@ -53,31 +59,44 @@ groups = {
 	'Leptons'  	: ['CMS_eff_[em]', 'CMS_eff_trigger_[em]'],
 	'JetMET'   	: ['CMS_*_j_13TeV', 'CMS_METunclustered_13TeV'],
 	'XSections' : ['CMS_httbar_*_13TeV'],
-	'QCDNorm'   : ['CMS_httbar_*_QCDNorm'],
+	'QCDNorm'   : ['CMS_httbar_QCD*Norm'],
 	'PU'        : ['CMS_pileup'],
 	'Theory'    : ['*_TT', 'TMass', 'pdf'],	
 }
 
+class Backup:
+	def __init__(self, flist):
+		self.backed_up = [backup(i) for i in flist]
+		print 'backed up %d files' % len(self.backed_up)
+
+	def __enter__(self):
+		return self
+
+	def __exit__(self, type, value, traceback):
+		[unbackup(i) for i in self.backed_up]
+		print 'restored %d files' % len(self.backed_up)
+		if isinstance(value, Exception):
+			raise value
+
 #backup files
-backed_up = [backup(i) for i in glob('[AH]_[0-9]*/000/higgsCombine.limit.Asymptotic.*.root')]
+#backed_up = [backup(i) for i in glob('[AH]_[0-9]*/000/higgsCombine.limit.Asymptotic.*.root')]
 
-#run limits
-for group, names in groups.iteritems():
-	print 'running for group: ', group
-	tofreeze = [i for i in nuisances if any((fnmatch(i, j) for j in names))]
-	retval = call(
-		('combineTool.py -M Asymptotic -m 400:750:50 --freezeNuisances MH,%s'
-		' -d */*/workspace.root --there -n .limit --parallel 8 %s') % \
-			(','.join(tofreeze), '--run blind' if args.run == 'blind' else '')
-		)
-	if retval != 0:
-		raise RuntimeError('combineTool.py crashed with exit code %d' % retval)
-	#tag outputs
-	[tag(i, group) for i in glob('[AH]_[0-9]*/000/higgsCombine.limit.Asymptotic.*.root')]
-	retval = call(
-		'combineTool.py -M CollectLimits */*/{0}_*.limit.* --use-dirs -o {0}.json'.format(group)
-		)
-	if retval != 0:
-		raise RuntimeError('limit harvesting crashed with exit code %d' % retval)
+with Backup(glob('[AH]_[0-9]*/[0-9][0-9][0-9]/higgsCombine.limit.Asymptotic.*.root')) as back:
+	#run limits
+	for group, names in groups.iteritems():
+		print 'running for group: ', group
+		tofreeze = [i for i in nuisances if any((fnmatch(i, j) for j in names))]
+		if not tofreeze:
+			raise RuntimeError('I could not find any nuisance matching the group %s (%s)' % (group, ', '.join(names)))
+		call(
+			('combineTool.py -M Asymptotic --freezeNuisances %s'
+			 ' -d */*/workspace.root --there -n .limit --parallel 8 %s') % \
+				(','.join(tofreeze), '--run blind' if args.run == 'blind' else '')
+			)
+	  #tag outputs
+		[tag(i, group) for i in glob('[AH]_[0-9]*/[0-9][0-9][0-9]/higgsCombine.limit.Asymptotic.*.root')]
+		call(
+			'combineTool.py -M CollectLimits */*/{0}_*.limit.* --use-dirs -o {0}.json'.format(group)
+			)
 
-[unbackup(i) for i in backed_up]
+#[unbackup(i) for i in backed_up]
