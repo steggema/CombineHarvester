93f20a90bcaff0b8ae59d70b9b476be4284b9f30
Jan Steggemann <jan.steggemann@cern.ch>
2017-05-31 16:35:10 +0200

    Update mass morphing to fine-binned 2D histograms

--- Httbar/scripts/morph_mass.py
+++ Httbar/scripts/morph_mass.py
@@ -6,28 +6,36 @@ from numpy import array
 
 import ROOT
 
-from ROOT import RooFit, TVectorD
+from ROOT import TVectorD
 from ROOT import RooDataHist, RooArgSet, RooArgList, RooMomentMorph, RooHistPdf
+
+ROOT.RooMsgService.instance().setGlobalKillBelow(ROOT.RooFit.WARNING)
 
 parser = ArgumentParser()
 parser.add_argument('inputfile')
-parser.add_argument('--hyperbolic', action='store_true', help='use hyperbolic interpolation')
-parser.add_argument('--algo', default='Linear', help="Choose morphing algo from 'Linear', 'NonLinear', 'NonLinearPosFractions', 'NonLinearLinFractions', 'SineLinear'")
-parser.add_argument('--input_masses', default='400,500,600,750', help='comma separated list of masses')
-parser.add_argument('--widths', default='2p5,5,10,25,50', help='comma separated list of widths')
+parser.add_argument('--hyperbolic', action='store_true',
+                    help='use hyperbolic interpolation')
+parser.add_argument('--algo', default='Linear',
+                    help="Choose morphing algo from 'Linear', 'NonLinear', 'NonLinearPosFractions', 'NonLinearLinFractions', 'SineLinear'")
+parser.add_argument('--input_masses', default='400,500,600,750',
+                    help='comma separated list of masses')
+parser.add_argument('--widths', default='2p5,5,10,25,50',
+                    help='comma separated list of widths')
 parser.add_argument('--stepsize', default='50')
-
-# FIXME - get these from some clever algorithm, not by hand...
-unc_names_mu = ['', '_CMS_eff_mUp', '_CMS_eff_mDown', '_CMS_pileupUp', '_CMS_pileupDown', '_CMS_eff_b_13TeVUp', '_CMS_eff_b_13TeVDown', '_CMS_fake_b_13TeVUp', '_CMS_fake_b_13TeVDown', '_CMS_scale_j_13TeVUp', '_CMS_scale_j_13TeVDown', '_CMS_res_j_13TeVUp', '_CMS_res_j_13TeVDown', '_CMS_METunclustered_13TeVUp', '_CMS_METunclustered_13TeVDown']
-unc_names_ele = ['', '_CMS_eff_eUp', '_CMS_eff_eDown', '_CMS_pileupUp', '_CMS_pileupDown', '_CMS_eff_b_13TeVUp', '_CMS_eff_b_13TeVDown', '_CMS_fake_b_13TeVUp', '_CMS_fake_b_13TeVDown', '_CMS_scale_j_13TeVUp', '_CMS_scale_j_13TeVDown', '_CMS_res_j_13TeVUp', '_CMS_res_j_13TeVDown', '_CMS_METunclustered_13TeVUp', '_CMS_METunclustered_13TeVDown']
 
 args = parser.parse_args()
 
-output_binning = [250.0, 360.0, 380.0, 400.0, 420.0, 440.0, 460.0, 480.0, 500.0, 520.0, 540.0, 560.0, 580.0, 610.0, 640.0, 680.0, 730.0, 800.0, 920.0, 1200.0]
+# ALL THE DANGEROUS CONSTANTS IN ONE PLACE
+OUTPUT_BINNING = [250.0, 360.0, 380.0, 400.0, 420.0, 440.0, 460.0, 480.0, 500.0,
+                  520.0, 540.0, 560.0, 580.0, 610.0, 640.0, 680.0, 730.0, 800.0, 920.0, 1200.0]
+N_REGIONS = 5
+MIN_MASS = 250.0
+MAX_MASS = 1200.0
 
 available = sorted([int(m) for m in args.input_masses.split(',')])
 stepsize = int(args.stepsize)
-to_make = [min(available) + (i + 1)*stepsize for i in xrange((max(available)-min(available))/stepsize)]
+to_make = [min(available) + (i + 1) *
+           stepsize for i in xrange((max(available) - min(available)) / stepsize)]
 to_make = [m for m in to_make if m not in available]
 print 'Output masses:', to_make
 
@@ -39,107 +47,185 @@ outname = args.inputfile.replace('.root', '_morphed_mass.root')
 infile = ROOT.TFile(args.inputfile)
 outfile = ROOT.TFile(outname, 'RECREATE')
 
-# m_ttbar = ROOT.RooRealVar('mass', 'mass', 0., 100.)
-m_ttbar = ROOT.RooRealVar('mass', 'mass', 250., 1200.) # FIXME - get these from the input histograms
-m_A = ROOT.RooRealVar('m_A', 'm_A', float(min(available)), float(max(available)))
-
+m_ttbar = ROOT.RooRealVar('mass', 'mass', MIN_MASS, MAX_MASS)
+m_A = ROOT.RooRealVar('m_A', 'm_A', float(
+    min(available)), float(max(available)))
 
 # Get mass interpolation from Viola's file
-file_int = ROOT.TFile(path.expandvars('$CMSSW_BASE/src/CombineHarvester/Httbar/data/Spin0_xsecs_vs_mass.root'))
+file_int = ROOT.TFile(path.expandvars(
+    '$CMSSW_BASE/src/CombineHarvester/Httbar/data/Spin0_xsecs_vs_mass.root'))
+file_int_int = ROOT.TFile(path.expandvars(
+    '$CMSSW_BASE/src/CombineHarvester/Httbar/data/Spin0_SEweight_and_NegEvtsFrac_vs_mass.root'))
 # pattern_translator = {'pos-sgn':'res', 'pos-int':'int'}
 
+
 def interpolate(masses, yields, test_mass, linear=True):
-	if not linear:
-		g = ROOT.TGraph(len(masses), array([float(m) for m in masses]), array(yields))
-		# import pdb; pdb.set_trace()
-		return g.Eval(float(test_mass), 0)
+    if not linear:
+        g = ROOT.TGraph(len(masses), array(
+            [float(m) for m in masses]), array(yields))
+        # import pdb; pdb.set_trace()
+        return g.Eval(float(test_mass), 0)
 
-	from bisect import bisect_left, bisect_right
-	try:
-		i_low = bisect_left(masses, test_mass) - 1
-		i_high = bisect_right(masses, test_mass)
-		m_below = masses[i_low]
-		m_above = masses[i_high]
-		y_below = yields[i_low]
-		y_above = yields[i_high]
-		if i_low == i_high:
-			import pdb; pdb.set_trace()
-	except:
-		import pdb; pdb.set_trace()
+    from bisect import bisect_left, bisect_right
 
-	delta_m = float(m_above - m_below)
-	f_low = (test_mass - m_below)/delta_m
-	f_high = (m_above - test_mass)/delta_m
-	return f_low*y_below + f_high*y_above
-	
+    i_low = bisect_left(masses, test_mass) - 1
+    i_high = bisect_right(masses, test_mass)
+    m_below = masses[i_low]
+    m_above = masses[i_high]
+    y_below = yields[i_low]
+    y_above = yields[i_high]
+    if i_low == i_high:
+        import pdb
+        pdb.set_trace()
+
+    delta_m = float(m_above - m_below)
+    f_low = (test_mass - m_below) / delta_m
+    f_high = (m_above - test_mass) / delta_m
+    return f_low * y_below + f_high * y_above
 
 
-# for channel in [i.GetName() for i in infile.GetListOfKeys()]:
-for channel in ['ejets', 'mujets']:
-	tdir = infile.Get(channel)
-	tdir.cd()
-	outfile.mkdir(channel)
-	for pattern in ['neg-int', 'pos-int', 'pos-sgn']:
-		for width in widths:
-			unc_names = unc_names_ele if channel == 'ejets' else unc_names_mu
+for channel in [i.GetName() for i in infile.GetListOfKeys()]:
+    tdir = infile.Get(channel)
+    tdir.cd()
+    outfile.mkdir(channel)
+    h_names = set([key.GetName().replace('400', '{MASS}').replace('500', '{MASS}').replace('600', '{MASS}').replace('750', '{MASS}') for key in tdir.GetListOfKeys()])
+    print 'Processing', len(h_names), 'different signal templates'
+    for h_name in h_names: # it's not very nice - somehow need to make sure we extract all the histogram name but the mass
+        pattern = h_name[4:11]
+        width = [w for w in widths if w+'pc' in h_name]
+        width = max(width, key=len) # e.g. for '2p5pc' and '5pc'
+        if not width:
+            print 'Did not find width in histogram name', h_name
+            import pdb; pdb.set_trace()
+            continue
 
-			# FIXME: use signal cross section for everything. Cant use interference
-			# since we need split into pos/neg parts
-			g_int = file_int.Get('A_res_semilep_w{}_toterr'.format(width))
+        if 'ggH' in h_name:
+            print 'FIXME - no ggH for the moment in the other signal files, continue'
+            continue
 
-			# unc_names = ['']
+        g_int = None
+        g_int_frac = None
+        if pattern == 'pos-sgn':
+            g_int = file_int.Get('A_res_semilep_w{}_toterr'.format(width))
 
-			for unc_name in unc_names:
+        if pattern in ['pos-int', 'neg-int']:
+            g_int = file_int_int.Get('A_int_semilep_w{}_SEweight'.format(width))
+            g_int_frac = file_int_int.Get('A_int_semilep_w{}_NegEvts_Frac'.format(width))
 
-				pdfs = RooArgList()
-				keeper = []
-				vd = TVectorD(len(available))
-				yields = []
-				for i_m, mass in enumerate(available):
-					h_ori = infile.Get('{channel}/ggA_{pattern}-{width}pc-M{mass}{unc_name}'.format(channel=channel, pattern=pattern, width=width, mass=mass, unc_name=unc_name))
-					yields.append(h_ori.Integral()/g_int.Eval(mass))
-					# print '{channel}/ggA_{pattern}-{width}pc-M{mass}{unc_name}'.format(channel=channel, pattern=pattern, width=width, mass=mass, unc_name=unc_name)
-					h_ori.Scale(1./g_int.Eval(mass))
-					# if pattern == 'neg-int':
-					# 	h_ori.Scale(-1.)
-					data_hist = RooDataHist('m'+h_ori.GetName(), '', RooArgList(m_ttbar), h_ori, 1.)
-					pdf = RooHistPdf('pdf_m'+h_ori.GetName(), '', RooArgSet(m_ttbar), data_hist)
-					pdfs.add(pdf)
-					vd[i_m] = float(mass)
-					keeper.append(pdf)
-					keeper.append(data_hist)
+        if not g_int:
+            import pdb; pdb.set_trace()
 
-				setting = getattr(ROOT.RooMomentMorph, args.algo)
-				morph = RooMomentMorph('morph'+channel+pattern, '', m_A, RooArgList(m_ttbar), pdfs, vd, setting)
-				# morph.Print('v')
+        # unc_names = ['']
 
-				print 'Yields', yields
-				
-				for test_mass in to_make:
-					m_A.setVal(float(test_mass))
+        mass_hists = {}
+        # First get the histograms for all masses
+        print '\n Processing:', h_name
+        for mass in available:
+            # print 'MASS', mass
+            # print '{channel}/{h_name}'.format(
+                # channel=channel, h_name=h_name)
+            h_ori = infile.Get('{channel}/{h_name}'.format(
+                channel=channel, h_name=h_name).format(MASS=mass)).Clone()
 
-					h_morph = h_ori.Clone('MORPH'+h_ori.GetName().replace(str(available[-1]), str(test_mass)))
+            h_ori.Scale(1. / g_int.Eval(mass))
+            if pattern == 'pos_int':
+                h_ori.Scale(1. / (1. - g_int_frac.Eval(mass)))
+            if pattern == 'neg_int':
+                h_ori.Scale(1. / g_int_frac.Eval(mass))
 
-					for i_bin in xrange(h_ori.GetNbinsX()):
+            # yields.append(h_ori.Integral())
 
-						# print test_mass
+            mass_hists[mass] = h_ori
 
-						x = h_ori.GetBinCenter(i_bin+1)
-						# print 'mttbar = ', x
-						m_ttbar.setVal(x)
+        d_hists_region = {}
 
-						# for i_pdf in xrange(len(pdfs)):
-						# 	print pdfs[i_pdf].getVal()
-						# print 'Morphed val:', morph.getVal()
+        # Split input histogram
+        # I DONT WANT TO DO THAT BUT OK
+        for i_costheta in xrange(N_REGIONS):
+            d_hists_region[i_costheta] = {}
+            pdfs = RooArgList()
+            keeper = []
+            vd = TVectorD(len(available))
+            yields = []
+            for i_m, mass in enumerate(available):
+                h_all = mass_hists[mass]
+                n_bins = h_all.GetNbinsX()
 
-						h_morph.SetBinContent(i_bin+1, morph.getVal()*h_ori.GetBinWidth(i_bin+1))
-					h_morph_rebin = h_morph.Rebin(len(output_binning)-1, h_morph.GetName().replace('MORPH', ''), array(output_binning))
-					outfile.cd()
-					outdir = outfile.Get(channel)
-					outdir.cd()
+                n_bins_region = n_bins/N_REGIONS
 
-					scale = interpolate(available, yields, test_mass)
-					h_morph_rebin.Scale(scale/h_morph_rebin.Integral()*g_int.Eval(mass))
-					# if pattern == 'neg-int':
-					# 	h_morph_rebin.Scale(-1.)
-					h_morph_rebin.Write()
+                h_region = ROOT.TH1D(channel+h_all.GetName()+str(i_costheta), '', n_bins_region, MIN_MASS, MAX_MASS)
+                yields.append(h_region.Integral())
+                for i_bin in xrange(n_bins_region):
+                    h_region.SetBinContent(i_bin+1, h_all.GetBinContent(i_bin+1 + i_costheta*n_bins_region))
+                    if h_all.GetBinContent(i_bin+1 + i_costheta*n_bins_region) < 0.:
+                        import pdb; pdb.set_trace()
+
+                # if pattern == 'neg-int':
+                # 	h_ori.Scale(-1.)
+                data_hist = RooDataHist(
+                    'm' + h_region.GetName(), '', RooArgList(m_ttbar), h_region, 1.)
+                pdf = RooHistPdf('pdf_m' + h_region.GetName(),
+                                 '', RooArgSet(m_ttbar), data_hist)
+                pdfs.add(pdf)
+                vd[i_m] = float(mass)
+                keeper.append(pdf)
+                keeper.append(data_hist)
+                keeper.append(h_region)
+
+            setting = getattr(ROOT.RooMomentMorph, args.algo)
+            morph = RooMomentMorph(
+                'morph' + channel + h_name.format(MASS='000'), '', m_A, RooArgList(m_ttbar), pdfs, vd, setting)
+            # morph.Print('v')
+
+            # print 'Yields', pattern, yields
+
+            for test_mass in to_make:
+                m_A.setVal(float(test_mass))
+
+                h_morph_region = h_region.Clone(
+                    'MORPH' + h_region.GetName().replace(str(available[-1]), str(test_mass)))
+
+                for i_bin in xrange(h_region.GetNbinsX()):
+
+                    # print test_mass
+
+                    x = h_region.GetBinCenter(i_bin + 1)
+                    # print 'mttbar = ', x
+                    m_ttbar.setVal(x)
+
+                    # for i_pdf in xrange(len(pdfs)):
+                    # 	print pdfs[i_pdf].getVal()
+                    # print 'Morphed val:', morph.getVal()
+
+                    h_morph_region.SetBinContent(
+                        i_bin + 1, morph.getVal() * h_morph_region.GetBinWidth(i_bin + 1))
+                h_morph_region_rebin = h_morph_region.Rebin(len(
+                    OUTPUT_BINNING) - 1, h_morph_region.GetName().replace('MORPH', ''), array(OUTPUT_BINNING))
+
+
+                scale = interpolate(available, yields, test_mass)
+                h_morph_region_rebin.Scale(
+                    scale / h_morph_region_rebin.Integral() * g_int.Eval(mass))
+                if pattern == 'pos-int':
+                    h_morph_region_rebin.Scale(1. - g_int_frac.Eval(mass))
+                if pattern == 'neg-int':
+                    h_morph_region_rebin.Scale(g_int_frac.Eval(mass))
+
+                d_hists_region[i_costheta][test_mass] = h_morph_region_rebin
+
+        # Now add the hists from the different regions for all masses
+        for test_mass in to_make:
+            n_bins_out = N_REGIONS*(len(OUTPUT_BINNING) - 1)
+            outfile.cd()
+            outdir = outfile.Get(channel)
+            outdir.cd()
+            h_out = ROOT.TH1D(d_hists_region[0][test_mass].GetName()[:-1].replace(channel, ''), '', n_bins_out, 0., float(n_bins_out))
+
+            for i_costheta in xrange(N_REGIONS):
+                h_part = d_hists_region[i_costheta][test_mass]
+                n_bins_part = h_part.GetNbinsX()
+                for i_bin in xrange(n_bins_part):
+                    h_out.SetBinContent(i_bin+1 + i_costheta*n_bins_part, h_part.GetBinContent(i_bin+1))
+
+            h_out.Write()
+            outfile.Write()
