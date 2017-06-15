77c207e07de14e0949a12438c6e828171e9740da
Jan Steggemann <jan.steggemann@cern.ch>
2017-06-15 14:25:54 +0200

    Update impact script

--- Httbar/scripts/impacts.sh
+++ Httbar/scripts/impacts.sh
@@ -9,8 +9,8 @@ for wdir in $(ls -d [AH]_[0-9]*); do
 	mkdir -p impact_plots
 	for subdir in $(ls -d [0-9]*); do
 		cd impacts
-		combineTool.py -M Impacts -d ../$subdir/workspace.root -m $subdir --doInitialFit --robustFit 1  -t -1 --expectSignal=1  --toysFreq
-		combineTool.py -M Impacts -d ../$subdir/workspace.root -m $subdir --robustFit 1 --doFits --parallel 4  -t -1 --expectSignal=1  --toysFreq
+		combineTool.py -M Impacts -d ../$subdir/workspace.root -m $subdir --doInitialFit --robustFit 1 -t -1 --expectSignal=1
+		combineTool.py -M Impacts -d ../$subdir/workspace.root -m $subdir --robustFit 1 --doFits --parallel 4 -t -1 --expectSignal=1
 		combineTool.py -M Impacts -d ../$subdir/workspace.root -m $subdir -o impacts_$subdir.json
 		plotImpacts.py -i impacts_$subdir.json -o impacts_$subdir
 		mv impacts_${subdir}.* ../impact_plots/
