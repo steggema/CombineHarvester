2de99017075cb3bf959c256b452982a4b31c2a1c
mauro verzetti <mverzett@cern.ch>
2017-07-02 03:13:10 -0500

    make it work on 2D histograms

--- Httbar/scripts/morph_mass.py
+++ Httbar/scripts/morph_mass.py
@@ -9,6 +9,7 @@ import ROOT
 
 from ROOT import TVectorD
 from ROOT import RooDataHist, RooArgSet, RooArgList, RooMomentMorph, RooHistPdf
+from pdb import set_trace
 
 ROOT.TH1.AddDirectory(False)
 
@@ -30,10 +31,9 @@ parser.add_argument('--interpolate', default='True')
 args = parser.parse_args()
 
 # ALL THE DANGEROUS CONSTANTS IN ONE PLACE
-OUTPUT_BINNING = [250.0, 360.0, 380.0, 400.0, 420.0, 440.0, 460.0, 480.0, 500.0,
-                  520.0, 540.0, 560.0, 580.0, 610.0, 640.0, 680.0, 730.0, 800.0, 920.0, 1200.0]
+OUTPUT_BINNING = [300.0, 340.0, 360.0, 380.0, 400.0, 420.0, 440.0, 460.0, 480.0, 500.0, 520.0, 540.0, 560.0, 580.0, 600.0, 625.0, 650.0, 675.0, 700.0, 730.0, 760.0, 800.0, 850.0, 900.0, 1000.0, 1200.0]
 N_REGIONS = 5
-MIN_MASS = 250.0
+MIN_MASS = 300.0
 MAX_MASS = 1200.0
 
 # Yield interpolation by hand (true) or via RooMorph (false)
@@ -95,7 +95,8 @@ for channel in [i.GetName() for i in infile.GetListOfKeys()]:
     tdir = infile.Get(channel)
     tdir.cd()
     outfile.mkdir(channel)
-    h_names = set([key.GetName().replace('400', '{MASS}').replace('500', '{MASS}').replace('600', '{MASS}').replace('750', '{MASS}') for key in tdir.GetListOfKeys()])
+    h_names = set([key.GetName().replace('400', '{MASS}').replace('500', '{MASS}').replace('600', '{MASS}').replace('750', '{MASS}') for key in tdir.GetListOfKeys()])		
+    #h_names = [i for i in h_names if not (i.endswith('Up') or i.endswith('Down'))] #FIXME
     print 'Processing', len(h_names), 'different signal templates'
     for h_name in h_names: # it's not very nice - somehow need to make sure we extract all the histogram name but the mass
         start = timer()
@@ -128,6 +129,7 @@ for channel in [i.GetName() for i in infile.GetListOfKeys()]:
             import pdb; pdb.set_trace()
 
         mass_hists = {}
+        mass_scales = {}
         # First get the histograms for all masses
 
         print '\n Processing:', h_name
@@ -136,21 +138,27 @@ for channel in [i.GetName() for i in infile.GetListOfKeys()]:
 
         for mass in available:
             # print 'MASS', mass
-            # print '{channel}/{h_name}'.format(
-                # channel=channel, h_name=h_name)
+            #print '{channel}/{h_name}'.format(
+            #    channel=channel, h_name=h_name).format(MASS=mass)
             h_ori = infile.Get('{channel}/{h_name}'.format(
-                channel=channel, h_name=h_name).format(MASS=mass)).Clone()
+                channel=channel, h_name=h_name).format(MASS=mass)).Clone(
+							h_name.format(MASS=mass)
+							)
+            #print 'MASS', mass, h_ori.GetName()
 
+            scale = 1. / g_int.Eval(mass)
             h_ori.Scale(1. / g_int.Eval(mass))
 
-
             if pattern == 'pos-int':
+                scale *= 1. / (1. - g_int_neg_frac.Eval(mass))
                 h_ori.Scale(1. / (1. - g_int_neg_frac.Eval(mass)))
 
             if pattern == 'neg-int':
+                scale *= 1. / g_int_neg_frac.Eval(mass)
                 h_ori.Scale(1. / g_int_neg_frac.Eval(mass))
             # yields.append(h_ori.Integral())
 
+            mass_scales[mass] = scale
             mass_hists[mass] = h_ori
 
         d_hists_region = {}
@@ -158,8 +166,14 @@ for channel in [i.GetName() for i in infile.GetListOfKeys()]:
         eval_time = 0.
         create_time = 0.
 
+        #set_trace()
         # Split input histogram
         # I DONT WANT TO DO THAT BUT OK
+        if mass_hists[available[0]].GetNbinsY() != N_REGIONS:
+            raise RuntimeError(
+							'the histogram %s has only %d y bins instead of %d expected' % \
+								(h_ori.GetName(), h_ori.GetNbinsY(), N_REGIONS)
+							)
         for i_costheta in xrange(N_REGIONS):
             d_hists_region[i_costheta] = {}
             pdfs = RooArgList()
@@ -168,26 +182,30 @@ for channel in [i.GetName() for i in infile.GetListOfKeys()]:
             yields = []
             for i_m, mass in enumerate(available):
                 h_all = mass_hists[mass]
-                n_bins = h_all.GetNbinsX()
-
-                n_bins_region = n_bins/N_REGIONS
-
                 tmp_file.cd()
-                h_region = ROOT.TH1D(channel+h_all.GetName()+str(i_costheta), '', n_bins_region, MIN_MASS, MAX_MASS)
-                
-                for i_bin in xrange(n_bins_region):
-                    h_region.SetBinContent(i_bin+1, h_all.GetBinContent(i_bin+1 + i_costheta*n_bins_region))
-                    if h_all.GetBinContent(i_bin+1 + i_costheta*n_bins_region) < 0.:
-                        import pdb; pdb.set_trace()
-
+                h_region = h_all.ProjectionX(
+									channel+h_all.GetName()+str(i_costheta)+"_finebinning",
+									i_costheta+1, i_costheta+1
+									)
+                d_hists_region[i_costheta][mass] = h_region.Rebin(
+									len(OUTPUT_BINNING) - 1, 
+									h_region.GetName().replace('_finebinning', ''), 
+									array(OUTPUT_BINNING)
+									)
+                d_hists_region[i_costheta][mass].Scale(1./mass_scales[mass])
                 yields.append(h_region.Integral())
 
                 # if pattern == 'neg-int':
                 # 	h_ori.Scale(-1.)
+                #set_trace()
                 data_hist = RooDataHist(
-                    'm' + h_region.GetName(), '', RooArgList(m_ttbar), h_region, 1.)
-                pdf = RooHistPdf('pdf_m' + h_region.GetName(),
-                                 '', RooArgSet(m_ttbar), data_hist)
+                    'm' + h_region.GetName().replace('_finebinning', ''), 
+										'', RooArgList(m_ttbar), h_region, 1.
+										)
+                pdf = RooHistPdf(
+									'pdf_m' + h_region.GetName().replace('_finebinning', ''),
+									'', RooArgSet(m_ttbar), data_hist
+									)
                 pdfs.add(pdf)
                 vd[i_m] = float(mass)
                 keeper.append(pdf)
@@ -200,12 +218,12 @@ for channel in [i.GetName() for i in infile.GetListOfKeys()]:
             # morph.Print('v')
 
             # print 'Yields', i_costheta, pattern, yields
-
             for test_mass in to_make:
                 m_A.setVal(float(test_mass))
                 tmp_file.cd()
+                #set_trace()
                 h_morph_region = h_region.Clone(
-                    'MORPH' + h_region.GetName().replace(str(available[-1]), str(test_mass)))
+                    'MORPH' + h_region.GetName().replace('_finebinning', '').replace(str(available[-1]), str(test_mass)))
 
                 for i_bin in xrange(h_morph_region.GetNbinsX()):
 
@@ -243,8 +261,8 @@ for channel in [i.GetName() for i in infile.GetListOfKeys()]:
             for item in keeper:
                 del item
         
-        # Now add the hists from the different regions for all masses
-        for test_mass in to_make:
+        # Now collate the hists from the different regions for all masses, including the available
+        for test_mass in to_make+available:
             n_bins_out = N_REGIONS*(len(OUTPUT_BINNING) - 1)
             outfile.cd()
             outdir = outfile.Get(channel)
@@ -259,6 +277,7 @@ for channel in [i.GetName() for i in infile.GetListOfKeys()]:
                     h_out.SetBinError(i_bin+1, 0.)
             h_out.Write()
             # print 'Writing', test_mass, h_out.Integral()
+
         tmp_file.Close()
         print 'Time elapsed:', timer() - start
     outfile.Write()
