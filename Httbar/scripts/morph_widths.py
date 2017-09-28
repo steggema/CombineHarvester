81e1ec9e9f477edd8342eab66fab68d3b1e8cb5d
mauro verzetti <mverzett@cern.ch>
2017-09-28 07:45:22 -0500

    probably alkaid's changes

--- Httbar/scripts/morph_widths.py
+++ Httbar/scripts/morph_widths.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from argparse import ArgumentParser
-
+import numpy as np
 parser = ArgumentParser()
 parser.add_argument('inputfile')
 parser.add_argument('--forchecks', action='store_true')
-parser.add_argument('--hyperbolic', action='store_true', help='use hyperbolic interpolation')
+#parser.add_argument('--hyperbolic', action='store_true', help='use hyperbolic interpolation')
 args = parser.parse_args()
 
 import ROOT
@@ -13,6 +13,7 @@ import re
 from pdb import set_trace
 
 mapping = {
+  '1pc' : 1.0,
 	'2p5pc' : 2.5,
 	'5pc'   : 5.0,
 	'10pc'  : 10.,
@@ -25,7 +26,7 @@ checks = {
 	10. : 'checks_10pc',
 	25. : 'checks_25pc',	
 }
-
+'''
 new_points = {
 	7.0 : '7pc' ,
 	15. : '15pc',
@@ -33,7 +34,11 @@ new_points = {
 	30. : '30pc',	
 	40. : '40pc',	
 }
-
+'''
+set_trace()
+new_points = dictionary = dict(zip(list(np.arange(2.5,50,0.5)),list("%s%s" % (str(x).replace('.','p').replace('p0',''),"pc") for x in np.arange(2.5,50,0.5))))
+for key in mapping.values():
+ new_points.pop(key,None)
 if args.forchecks:
 	new_points.update(checks)
 
@@ -53,7 +58,7 @@ for point in to_make:
 
 def get_info(name):
 	try:
-		process, sigint, width, mass_sys = tuple(name.split('-'))
+		process, sigint, width, mass_sys = tuple(name.split('-',3))
 	except:
 		set_trace()
 	key = (process, sigint, mass_sys)
@@ -66,32 +71,39 @@ def make_name(key, width):
 outname = args.inputfile.replace('.root', '_morphed.root')
 shutil.copyfile(args.inputfile, outname)
 infile = ROOT.TFile(outname, 'UPDATE')
-
 for category in [i.GetName() for i in infile.GetListOfKeys()]:
+	counter = 0
 	tdir = infile.Get(category)
 	tdir.cd()
 	shapes = [i.GetName() for i in tdir.GetListOfKeys()]
 	shapes = [i for i in shapes if i.startswith('ggA_') or i.startswith('ggH_')]
+	nMorph = len(shapes)*len(new_points)
+	counter = 0
+	shapes = { "sgn" : (i for i in shapes if '_pos-sgn' in i), "int" : (i for i in shapes if '-int-' in i)}
 	#there are still a lot of spurious shapes, remove them
-	shapes = [i for i in shapes if '_pos-sgn' in i or '-int-' in i]
-	shapes_map = {}
-	for shape in shapes:
-		k, w = get_info(shape)
-		if k not in shapes_map:
-			shapes_map[k] = {}
-		shapes_map[k][w] = tdir.Get(shape)
-
+	shapes_map = { "sgn": {}, "int": {}}
+	hyperbolic = { "sgn": True, "int": False}
+	for _type in shapes:
+	 for shape in shapes[_type]:
+	  k, w = get_info(shape)
+	  if k not in shapes_map[_type]:
+	   shapes_map[_type][k] = {}
+	  shapes_map[_type][k][w] = tdir.Get(shape)
 	#compute new histograms
-	for key in shapes_map:
-		for width in new_points:
-			b, a = interpolation[width]
-			factor = (width-b)/(a-b)
-			if args.hyperbolic:
-				factor = (1./width-1./b)/(1./a-1./b)
-			below = shapes_map[key][b]
-			above = shapes_map[key][a]
-			new_name = make_name(key, width)
-			new_hist = below.Clone(new_name)
-			new_hist.Scale(1-factor)
-			new_hist.Add(above, factor)
-			new_hist.Write()
+	for _type in shapes_map:
+	 for key in shapes_map[_type]:
+	  for width in new_points:
+	   b, a = interpolation[width]
+	   if hyperbolic[_type]:
+	    factor = (1./width-1./b)/(1./a-1./b)
+	   else:
+	    factor = (width-b)/(a-b)
+	   below = shapes_map[_type][key][b]
+	   above = shapes_map[_type][key][a]
+	   new_name = make_name(key, width)
+	   new_hist = below.Clone(new_name)
+	   new_hist.Scale(1-factor)
+	   new_hist.Add(above, factor)
+	   new_hist.Write()
+	   counter += 1
+	   print "Category-{} done: {}/{}".format(category,counter,nMorph)
