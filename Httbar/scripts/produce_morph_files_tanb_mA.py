8c3aeee631cbc8d5b9981a7c69c3d9ee02fb3adf
mauro verzetti <mverzett@cern.ch>
2017-11-07 17:27:13 +0100

    added batch hMSSM submission

--- Httbar/scripts/produce_morph_files_tanb_mA.py
+++ Httbar/scripts/produce_morph_files_tanb_mA.py
@@ -7,8 +7,11 @@ and widths, using SusHi/2HDMC results as input.
 import os
 import pickle
 from argparse import ArgumentParser
+import shutil
+from glob import glob
 
 parser = ArgumentParser()
+parser.add_argument('jobid')
 parser.add_argument('mA', type=int)
 parser.add_argument('tanb')
 parser.add_argument('input_sushi')
@@ -47,13 +50,13 @@ with open(args.input_sushi) as sushi_pkl:
 	print '  widthH =', widthH
 	print '  mH =', mH
 	
-	os.system('make_point.sh 2017Sep28 TESTME A:{}:{} H:{}:{}'.format(mA, widthA, mH, widthH))
+	os.system('make_point.sh {} TESTME A:{}:{} H:{}:{}'.format(args.jobid, mA, widthA, mH, widthH))
 	os.system(
 		'hadd -f templates_ALL_POINT.root TESTME.root '
 		'%s/src/CombineHarvester/Httbar/data/templates_l?_bkg_2017Aug04.root' % os.environ['CMSSW_BASE']
 		)
 	os.system((
-			'setup_common.py POINT --indir=./ --limitdir=limit'
+			'setup_common.py POINT --indir=./ --limitdir=./'
 			' --masses="A:{},H:{}" --widths="A:{},H:{}"').format(
 			mA, mH, val2name(widthA), val2name(widthH)
 			))
@@ -72,3 +75,12 @@ with open(args.input_sushi) as sushi_pkl:
 			'A_{}_{}_H_{}_{}/higgsCombine.limit.Asymptotic.mH120.root').format(
 			val2name(widthA), mA, val2name(widthH), mH, '--run blind' if args.blind else ''
 			))
+shutil.move('limits.json', 'mA%d_tanb%.2f.json' % (mA, tanb))
+shutil.rmtree(
+        'A_{}_{}_H_{}_{}'.format(
+                val2name(widthA), mA, val2name(widthH), mH, '--run blind' if args.blind else ''
+                )
+        )
+for fname in glob('*.root'):
+        shutil.rmtree(fname)
+
