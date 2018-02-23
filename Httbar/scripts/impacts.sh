5ecdf4c9953b3622356e926e8cd5d7e2706392d4
mauro verzetti <mverzett@cern.ch>
2018-02-23 17:33:37 +0100

    fix to work on the workflow

--- Httbar/scripts/impacts.sh
+++ Httbar/scripts/impacts.sh
@@ -7,10 +7,19 @@ tarfile=$1
 mass=$2
 filename="${tarfile%.*}"
 
+echo 'creating directory'
 mkdir -p impacts_$filename
 cp $tarfile impacts_$filename/.
+cd impacts_$filename
+echo 'untarring files'
 tar -xf $tarfile
-combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --doInitialFit --robustFit 1 -t -1 --expectSignal=1
-combineTool.py -M Impacts -d ../$mass/workspace.root -m $mass --robustFit 1 --doFits --parallel 8 -t -1 --expectSignal=1
-combineTool.py -M Impacts -d ../$mass/workspace.root -m $mass -o impacts_$mass.json
+
+echo 'initial fit'
+combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --doInitialFit --robustFit 1 -t -1 --expectSignal=1 &> initial_fit.log
+echo 'impacts'
+combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --robustFit 1 --doFits --parallel 8 -t -1 --expectSignal=1 &> impacts.log
+echo 'making json'
+combineTool.py -M Impacts -d */$mass/workspace.root -m $mass -o impacts_$mass.json
+echo 'making plots'
 plotImpacts.py -i impacts_$mass.json -o impacts_$mass
+cd -
