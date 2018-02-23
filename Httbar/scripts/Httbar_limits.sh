336dad0d7cbb220c6232a6d67f70ffbcee398b38
mauro verzetti <mverzett@cern.ch>
2018-02-23 17:35:45 +0100

    change fitting parameters

--- Httbar/scripts/Httbar_limits.sh
+++ Httbar/scripts/Httbar_limits.sh
@@ -4,5 +4,5 @@ set -o nounset
 set -o errexit
 
 #for wdir in $(ls -d [AH]_[0-9]*); do 
-combineTool.py -M Asymptotic -d */*/workspace.root --there -n .limit --minimizerTolerance=0.0001 --minimizerStrategy=2 --parallel 10 $@
+combineTool.py -M Asymptotic -d */*/workspace.root --there -n .limit --minimizerTolerance=0.1 --minimizerStrategy=1 --parallel 10 --rMin=0 --rMax=4 $@
 #done
