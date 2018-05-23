3095dc2cee6691bc7b33c78193e5ef401fcebbb8
Jan Steggemann <jan.steggemann@cern.ch>
2018-05-23 10:19:23 +0200

    Settings for combine 8

--- Httbar/scripts/Httbar_limits.sh
+++ Httbar/scripts/Httbar_limits.sh
@@ -4,5 +4,5 @@ set -o nounset
 set -o errexit
 
 #for wdir in $(ls -d [AH]_[0-9]*); do 
-combineTool.py -M Asymptotic -d */*/workspace.root --there -n .limit --minimizerTolerance=0.1 --minimizerStrategy=1 --parallel 10 --rMin=0 --rMax=4 $@
+combineTool.py -M AsymptoticLimits -d */*/workspace.root --there -n .limit --parallel 10 --rMin=0 --rMax=4 $@
 #done
