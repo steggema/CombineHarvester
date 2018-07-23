059d09d32efc40c06e6ad4268daa7e3772c7f48c
Jan Steggemann <jan.steggemann@cern.ch>
2018-07-23 11:04:00 +0200

    Update impact scripts for two-parameter model

--- Httbar/scripts/impacts_unblind.sh
+++ Httbar/scripts/impacts_unblind.sh
@@ -19,15 +19,15 @@ echo 'untarring files'
 tar -xf $tarfile
 
 echo 'initial fit'
-combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --doInitialFit --robustFit 1 -t -1 --expectSignal=$coupling &> initial_fit.log
+combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --doInitialFit --robustFit 1  --expectSignal=$coupling &> initial_fit.log
 if [ "$filter" == "UNSET" ]; then
 		echo 'impacts'
-		combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --robustFit 1 --cminPreScan --doFits --parallel 8 -t -1 --expectSignal=1 --setParameters g=$coupling --freezeParameters g  &> impacts.log
+		combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --robustFit 1 --cminPreScan --doFits --parallel 8  --expectSignal=1 --setParameters g=$coupling --freezeParameters g > impacts.log
 		echo 'making json'
 		combineTool.py -M Impacts -d */$mass/workspace.root -m $mass -o impacts_$mass.json
 else
 		echo 'impacts'
-		combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --robustFit 1 --cminPreScan --doFits --parallel 8 -t -1 --expectSignal=1 --setParameters g={$coupling} --freezeParameters g  --filter=$filter &> impacts.log		
+		combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --robustFit 1 --cminPreScan --doFits --parallel 8  --expectSignal=1 --setParameters g=$coupling --freezeParameters g  --filter=$filter &> impacts.log		
 		echo 'making json'
 		combineTool.py -M Impacts -d */$mass/workspace.root -m $mass -o impacts_$mass.json --filter=$filter
 fi
