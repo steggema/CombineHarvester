b662165ef10c5534a81dff3d3a33f184735d9aeb
Jan Steggemann <jan.steggemann@cern.ch>
2018-07-25 14:48:45 +0200

    Minor fixes for 2-parameter impacts

--- Httbar/scripts/impacts_unblind.sh
+++ Httbar/scripts/impacts_unblind.sh
@@ -19,17 +19,17 @@ echo 'untarring files'
 tar -xf $tarfile
 
 echo 'initial fit'
-combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --doInitialFit --robustFit 1  --expectSignal=$coupling &> initial_fit.log
+combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --cminPreScan --X-rtd MINIMIZER_analytic --doInitialFit --robustFit 1  --expectSignal=1 --setParameters g=$coupling --freezeParameters g --redefineSignalPOIs r &> initial_fit.log
 if [ "$filter" == "UNSET" ]; then
 		echo 'impacts'
-		combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --robustFit 1 --cminPreScan --doFits --parallel 8  --expectSignal=1 --setParameters g=$coupling --freezeParameters g > impacts.log
+		combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --X-rtd MINIMIZER_analytic  --robustFit 1 --cminPreScan --doFits --parallel 8 --setParameters g=$coupling --freezeParameters g --redefineSignalPOIs r > impacts.log
 		echo 'making json'
-		combineTool.py -M Impacts -d */$mass/workspace.root -m $mass -o impacts_$mass.json
+		combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --redefineSignalPOIs r -o impacts_$mass.json
 else
 		echo 'impacts'
-		combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --robustFit 1 --cminPreScan --doFits --parallel 8  --expectSignal=1 --setParameters g=$coupling --freezeParameters g  --filter=$filter &> impacts.log		
+		combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --X-rtd MINIMIZER_analytic  --robustFit 1 --cminPreScan --doFits --parallel 8 --setParameters g=$coupling --freezeParameters g --redefineSignalPOIs r --filter=$filter &> impacts.log		
 		echo 'making json'
-		combineTool.py -M Impacts -d */$mass/workspace.root -m $mass -o impacts_$mass.json --filter=$filter
+		combineTool.py -M Impacts -d */$mass/workspace.root -m $mass --redefineSignalPOIs r -o impacts_$mass.json --filter=$filter
 fi
 echo 'making plots'
 plotImpacts.py -i impacts_$mass.json -o impacts_$mass
