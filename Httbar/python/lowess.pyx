5584ec04ff67bcfb09cd95b4d3d6e2fc6a867f84
mauro verzetti <mverzett@cern.ch>
2018-11-06 13:52:36 +0100

    PRELIMINARY: template smoothing implementation - still to define bandwidth

---
+++ Httbar/python/lowess.pyx
@@ -0,0 +1,387 @@
+"""Module provides functions for LOWESS smoothing [1].
+
+[1] https://en.wikipedia.org/wiki/Local_regression
+"""
+
+
+import numpy as np
+
+from numpy cimport float64_t 
+
+cimport cython
+from libc.math cimport sqrt
+
+
+def lowess(x, y, bandwidth):
+    """Smooth a function using LOWESS algorithm.
+    
+    Parameters:
+        x:  Sorted array of x coordinates.
+        y:  Corresponding values of the function.
+        bandwidth: Bandwith that defines the size of the neighbourhood
+            to be considered for each point.
+    
+    Return value:
+        An array with results of the smoothing at the same x coordinates
+        as given in inputs.
+    
+    At each point perform a linear regression.  Only points that fall
+    inside a window with a half-size equal to the given bandwidth are
+    considered.  The least-squares fit is performed using weights
+    computed by the tricube function of the distance in x to the central
+    point in the window, divided by the bandwith.
+    
+    This function is similar to implementation from statsmodels [1] but
+    operates with a fixed bandwith instead of a fixed number of
+    neighbours.
+    
+    [1] http://www.statsmodels.org/dev/generated/statsmodels.nonparametric.smoothers_lowess.lowess.html
+    """
+    
+    x = np.asarray(x)
+    smoothY = np.zeros(len(y))
+    
+    for i in range(len(y)):
+        
+        # Find points whose distance from x[i] is not greater than
+        # bandwidth.  The last point is not included in the range.
+        start = np.searchsorted(x, x[i] - bandwidth, side='left')
+        end = np.searchsorted(x, x[i] + bandwidth, side='right')
+        
+        
+        # Compute weights for selected points.  They are evaluated as
+        # the tricube function of the distance, in units of bandwidth.
+        # Negative weights are clipped.
+        distances = np.abs(x[start:end] - x[i]) / bandwidth
+        weights = (1 - distances**3)**3
+        weights[weights < 0.] = 0.
+        
+        # To simplify computation of mean values below, normalize
+        # weights
+        weights /= np.sum(weights)
+        
+        
+        # Perform linear fit to selected points.  The range is centered
+        # at x[i] so that only the constant term needs to be computed.
+        # This also improves numerical stability.  The computation is
+        # carried using an analytic formula.
+        xFit = x[start:end] - x[i]
+        
+        meanX = np.dot(weights, xFit)
+        meanY = np.dot(weights, y[start:end])
+        meanX2 = np.dot(weights, xFit**2)
+        meanXY = np.dot(weights, xFit * y[start:end])
+        
+        smoothY[i] = (meanX2 * meanY - meanX * meanXY) / (meanX2 - meanX**2)
+    
+    return smoothY
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+def lowess2d(x, y, bandwidth):
+    """Smooth a function of 2D data using LOWESS algorithm.
+    
+    Arguments:
+        x:  array_like of shape (n, 2)
+            Sequence of n 2D points.  Does not need to be ordered.
+        y:  array_like of length n
+            Input values of the fuction evaluated at each point.
+        bandwidth: array_like of length 2
+            Bandwidths to be used for each coordinate.
+    
+    Return value:
+        A NumPy array with smoothed values of the function.
+    
+    For each point, fit data in its neighbourhood with a plane,
+    weighting down points that are far away.  The smoothed value is
+    computed as the coordinate of the plane at that central point.
+    """
+    
+    # Create memory views of input arrays.  Convert them to float64 if
+    # needed.
+    x_np = np.asarray(x, dtype=np.float64)
+    y_np = np.asarray(y, dtype=np.float64)
+    
+    if x_np.shape[1] != 2:
+        raise RuntimeError('Size of array x along axis 1 must be 2.')
+    
+    if x_np.shape[0] != y_np.shape[0]:
+        raise RuntimeError('Dimensions of arrays x and y do not match.')
+    
+    cdef float64_t [:, :] x_v = x_np
+    cdef float64_t [:] y_v = y_np
+    
+    cdef float64_t h[2]
+    h[0], h[1] = bandwidth
+    
+    
+    ySmooth_np = np.empty_like(y_np)
+    cdef float64_t [:] ySmooth = ySmooth_np
+    
+    cdef:
+        Py_ssize_t nPoints = len(y)
+        Py_ssize_t iCentral, iCurrent, d
+        float64_t xTrans[2]
+        float64_t distance2, weight, det
+        float64_t sumW, sumWX0X1, sumWY
+        float64_t sumWX[2]
+        float64_t sumWXQ[2]
+        float64_t sumWXY[2]
+        float64_t m[3][3]
+    
+    
+    # Loop over given points
+    for iCentral in range(nPoints):
+        
+        sumW = sumWX0X1 = sumWY = 0.
+        
+        for d in range(2):
+            sumWX[d] = 0.
+            sumWXQ[d] = 0.
+            sumWXY[d] = 0.
+        
+        
+        # Compute various weighted sums looping over all points
+        for iCurrent in range(nPoints):
+            
+            # Centre x coordinates at the current point and express them
+            # in units of bandwidths along each axis.  The centring will
+            # allow to compute only one of the three parameters defining
+            # a plane.
+            for d in range(2):
+                xTrans[d] = (x_v[iCurrent, d] - x_v[iCentral, d]) / h[d]
+            
+            distance2 = xTrans[0]**2 + xTrans[1]**2
+            
+            if distance2 > 1:
+                continue
+            
+            
+            weight = (1 - sqrt(distance2)**3)**3
+            
+            sumW += weight
+            sumWX0X1 += weight * xTrans[0] * xTrans[1]
+            sumWY += weight * y_v[iCurrent]
+            
+            for d in range(2):
+                sumWX[d] += weight * xTrans[d]
+                sumWXQ[d] += weight * xTrans[d]**2
+                sumWXY[d] += weight * xTrans[d] * y_v[iCurrent]
+        
+        
+        # Set smoothed y at the current point to the constant term in
+        # the equation for the plane that fits the data in the local
+        # neighbourhood.  Use weighted least squares fit.  The resulting
+        # system of linear equations for parameters of the plane is
+        # solved using Cramer's rule.
+        m[0][0] = 1.
+        m[1][1] = sumWXQ[0] / sumW
+        m[2][2] = sumWXQ[1] / sumW
+        m[0][1] = m[1][0] = sumWX[0] / sumW
+        m[0][2] = m[2][0] = sumWX[1] / sumW
+        m[1][2] = m[2][1] = sumWX0X1 / sumW
+        
+        det = determinant_3x3(m)
+        
+        m[0][0] = sumWY / sumW
+        m[1][0] = sumWXY[0] / sumW
+        m[2][0] = sumWXY[1] / sumW
+        
+        ySmooth[iCentral] = determinant_3x3(m) / det
+    
+    
+    return ySmooth_np
+
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+def lowess2d_grid(x0, x1, y, bandwidth):
+    """Smooth a function of 2D data using LOWESS algorithm.
+    
+    Input values for the function are given on a grid.
+    
+    Arguments:
+        x0, x1:  array_like
+            Sorted arrays that define the grid along the two axes.
+        y:  array_like
+            2D array with input values of the fuction at each point on
+            the grid.  Axis 0 corresponds to x0, axis 1 to x1.
+        bandwidth: array_like of length 2
+            Bandwidths to be used for each coordinate.
+    
+    Return value:
+        A NumPy array with smoothed values of the function.
+    
+    For each point, fit data in its neighbourhood with a plane,
+    weighting down points that are far away.  The smoothed value is
+    computed as the coordinate of the plane at that central point.
+    """
+    
+    # Create memory views of input arrays.  Convert them to float64 if
+    # needed.
+    x0_np = np.asarray(x0, dtype=np.float64)
+    x1_np = np.asarray(x1, dtype=np.float64)
+    y_np = np.asarray(y, dtype=np.float64)
+    
+    if y_np.shape[0] != x0_np.shape[0] or y_np.shape[1] != x1_np.shape[0]:
+        raise RuntimeError('Dimenstions of array y do not match those of x0 and x1')
+    
+    cdef float64_t [:] x0_v = x0_np
+    cdef float64_t [:] x1_v = x1_np
+    cdef float64_t [:, :] y_v = y_np
+    
+    cdef float64_t h[2]
+    h[0] = bandwidth[0]
+    h[1] = bandwidth[1]
+    
+    
+    ySmooth_np = np.empty_like(y_np)
+    cdef float64_t [:,:] ySmooth = ySmooth_np
+    
+    cdef Py_ssize_t n[2]
+    n[0] = y_np.shape[0]
+    n[1] = y_np.shape[1]
+    
+    cdef:
+        Py_ssize_t iCentral_0
+        Py_ssize_t iCentral_1
+        Py_ssize_t iStart[2]
+        Py_ssize_t iEnd[2]
+        Py_ssize_t iCurrent_0
+        Py_ssize_t iCurrent_1
+        int d
+        
+        float64_t xTrans[2]
+        float64_t distance2, weight, det
+        
+        float64_t sumW, sumWX0X1, sumWY
+        float64_t sumWX[2]
+        float64_t sumWXQ[2]
+        float64_t sumWXY[2]
+        float64_t m[3][3]
+    
+    
+    for iCentral_0 in range(n[0]):
+        
+        # The window along the first coordinate around the current
+        # point.  Points with end indices are not included.
+        iStart[0] = upper_bound(x0_v, 0, iCentral_0, x0_v[iCentral_0] - h[0])
+        iEnd[0] = upper_bound(x0_v, iCentral_0, n[0], x0_v[iCentral_0] + h[0])
+        
+        for iCentral_1 in range(n[1]):
+            
+            # The window along the second coordinate
+            iStart[1] = upper_bound(x1_v, 0, iCentral_1, x1_v[iCentral_1] - h[1])
+            iEnd[1] = upper_bound(x1_v, iCentral_1, n[1], x1_v[iCentral_1] + h[1])
+            
+            
+            sumW = sumWX0X1 = sumWY = 0.
+            
+            for d in range(2):
+                sumWX[d] = 0.
+                sumWXQ[d] = 0.
+                sumWXY[d] = 0.
+            
+            # Loop over points included in the 2D window and compute
+            # weighted sums
+            for iCurrent_0 in range(iStart[0], iEnd[0]):
+                for iCurrent_1 in range(iStart[1], iEnd[1]):
+                    
+                    # Centre x coordinates at the current point and
+                    # express them in units of bandwidths along each
+                    # axis.  The centring will allow to compute only one
+                    # of the three parameters defining a plane.
+                    xTrans[0] = (x0_v[iCurrent_0] - x0_v[iCentral_0]) / h[0]
+                    xTrans[1] = (x1_v[iCurrent_1] - x1_v[iCentral_1]) / h[1]
+                    
+                    distance2 = xTrans[0]**2 + xTrans[1]**2
+                    
+                    if distance2 > 1:
+                        continue
+                    
+                    
+                    weight = (1 - sqrt(distance2)**3)**3
+                    
+                    sumW += weight
+                    sumWX0X1 += weight * xTrans[0] * xTrans[1]
+                    sumWY += weight * y_v[iCurrent_0, iCurrent_1]
+                    
+                    for d in range(2):
+                        sumWX[d] += weight * xTrans[d]
+                        sumWXQ[d] += weight * xTrans[d]**2
+                        sumWXY[d] += weight * xTrans[d] * y_v[iCurrent_0, iCurrent_1]
+            
+            
+            # Set smoothed y at the current point to the constant term
+            # in the equation for the plane that fits the data in the
+            # local neighbourhood.  Use weighted least squares fit.  The
+            # resulting system of linear equations for parameters of the
+            # plane is solved using Cramer's rule.
+            m[0][0] = 1.
+            m[1][1] = sumWXQ[0] / sumW
+            m[2][2] = sumWXQ[1] / sumW
+            m[0][1] = m[1][0] = sumWX[0] / sumW
+            m[0][2] = m[2][0] = sumWX[1] / sumW
+            m[1][2] = m[2][1] = sumWX0X1 / sumW
+            
+            det = determinant_3x3(m)
+            
+            m[0][0] = sumWY / sumW
+            m[1][0] = sumWXY[0] / sumW
+            m[2][0] = sumWXY[1] / sumW
+            
+            ySmooth[iCentral_0, iCentral_1] = determinant_3x3(m) / det
+    
+    return ySmooth_np
+
+
+
+cdef float64_t determinant_3x3(float64_t m[3][3]):
+    """Compute determinant of 3x3 matrix using explicit formula."""
+    
+    return m[0][0] * (m[1][1] * m[2][2] - m[1][2] * m[2][1]) - \
+        m[0][1] * (m[1][0] * m[2][2] - m[1][2] * m[2][0]) + \
+        m[0][2] * (m[1][0] * m[2][1] - m[1][1] * m[2][0])
+
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef int upper_bound(float64_t [:] array, int start, int end, float64_t x):
+    """Find index of the first element that is bigger than x.
+    
+    Arguments:
+        array:  Sorted array.
+        start, end:  Indices that define a subrange of the array to be
+            considered.  Index 'end' is not included in the range.
+        x:  Value to compare against.
+    
+    Return value:
+        Index of the first element of the array, within the given
+        subrange, that is strictly bigger than x.  If x is bigger or
+        equal to the last element in the subrange, 'end' is returned.
+    
+    Implemented with a binary search.
+    """
+    
+    if x < array[start]:
+        return start
+    elif x >= array[end - 1]:
+        return end
+    
+    cdef int middle
+    
+    while end - start > 1:
+        
+        middle = start + (end - start) // 2
+        
+        if array[middle] > x:
+            end = middle
+        else:
+            start = middle
+    
+    return end
---
+++ Httbar/scripts/smooth_templates.py
@@ -0,0 +1,313 @@
+#!/usr/bin/env python
+
+"""Applies dummy smoothing to JME variations in signal templates.
+
+Stores in the output file only templates for the given signal
+hypothesis.
+"""
+
+import argparse
+import itertools
+import math
+import os
+import re
+
+import numpy as np
+
+import matplotlib as mpl
+mpl.use('agg')
+from matplotlib import pyplot as plt
+
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+from CombineHarvester.Httbar.smoothutils import Smoother
+from pdb import set_trace
+
+class Reader:
+    """Implements reading of signal templates.
+    
+    Templates are represented by NumPy arrays, whose axes have the
+    following meaning: (0) channel, (1) bin in angle, (2) bin in mass,
+    (3) histogram bin content versus squared uncertainty.  Input
+    templates are expected to be unrolled into 1D.
+    """
+    
+    def __init__(self, src_file, channels, num_bins_angle=5, example='TT', epsilon=10**-7):
+        
+        self.src_file = src_file
+        self.channels = channels
+        
+        # Extract number of bins in mass from an example template
+        hist = self.src_file.Get(channels[0] + '/' + example) #FIXME!
+        self.num_bins_mass = hist.GetNbinsX() // num_bins_angle
+        self.num_bins_angle = num_bins_angle
+        self.epsilon = epsilon
+    
+    
+    def read_counts(self, name):
+        """Read requested templates for all channels."""
+        
+        counts = np.empty((len(self.channels), self.num_bins_angle, self.num_bins_mass, 2))
+        
+        for ichannel, channel in enumerate(self.channels):
+            read_name = '{}/{}'.format(channel, name)
+            template = self.src_file.Get(read_name)
+            
+            if not template:
+                raise RuntimeError('Failed to read template "{}".'.format(read_name))
+            
+            counts[ichannel] = self._hist_to_array(template)
+        
+        return counts
+    
+    
+    def _hist_to_array(self, hist):
+        """Convert unrolled ROOT histogram into NumPy representation."""
+        
+        counts_flat = np.empty((hist.GetNbinsX(), 2))
+        
+        for i in range(len(counts_flat)):
+            if hist.GetBinError(i + 1) == 0 and hist.GetBinContent(i + 1) != 0:
+                raise RuntimeError('This will create problems downstream')
+            counts_flat[i, 0] = hist.GetBinContent(i + 1)
+            counts_flat[i, 1] = hist.GetBinError(i + 1) ** 2 + self.epsilon            
+        
+        return counts_flat.reshape(self.num_bins_angle, self.num_bins_mass, 2)
+
+
+class DummyRebinner:
+    
+    def __init__(self):
+        pass
+    
+    def __call__(self, array):
+        return array
+
+
+if __name__ == '__main__':
+    ROOT.gROOT.SetBatch(True)
+    
+    arg_parser = argparse.ArgumentParser(__doc__)
+    arg_parser.add_argument('input', help='ROOT file with original templates')
+    arg_parser.add_argument('point', help='point code A/H:MASS:WIDTH')
+    arg_parser.add_argument('badwidth', help='badwith json/dataframe')
+    arg_parser.add_argument(
+        '-o', '--output', default='smooth_sgn.root',
+        help='Name for output ROOT file with smoothed variations in signal'
+    )
+    arg_parser.add_argument(
+        '--fig-dir', default='', help='Directory for plots, if empty, no plots'
+    )
+    args = arg_parser.parse_args()
+    
+    parity, mass, width = tuple(args.point.split(':'))    
+    width = float(width)
+    width_mapping = {
+        .1 : '0p1pc',
+        1.0 : '1pc ' ,
+        2.5 : '2p5pc',
+        5.0 : '5pc'  ,
+        10. : '10pc' ,
+        25. : '25pc' ,
+        50. : '50pc' , 
+        }
+
+    sgn_template = 'gg%s_{}-%s-M%s' % (parity, width_mapping[width], mass)
+    sgn_name_regex = re.compile(
+        sgn_template.format(
+            '(pos|neg)-(sgn|int)'
+            )
+        )
+        
+    if args.fig_dir:
+        try:
+            os.makedirs(args.fig_dir)
+        except FileExistsError:
+            pass
+    
+    
+    sgn_types = {'pos-sgn': '$R$', 'pos-int': '$I^{+}$', 'neg-int': '$I^{-}$'}
+    
+    # Define variations to be smoothed.  Other templates will be copied
+    # to the output file directly
+    syst_names = []
+    
+    for jec_syst in [
+        'AbsoluteStat', 'AbsoluteScale', 'AbsoluteMPFBias', 'Fragmentation',
+        'SinglePionECAL', 'SinglePionHCAL', 'FlavorQCD', 'TimePtEta',
+        'RelativeJEREC1', 'RelativePtBB', 'RelativePtEC1', 'RelativeBal', 'RelativeFSR',
+        'RelativeStatFSR', 'RelativeStatEC',
+        'PileUpDataMC', 'PileUpPtRef', 'PileUpPtBB', 'PileUpPtEC1'
+    ]:
+        syst_names.append(('JEC' + jec_syst, 'CMS_scale_j_13TeV_' + jec_syst))
+    
+    syst_names.append(('JER', 'CMS_res_j_13TeV'))
+    syst_names.append(('METUncl', 'CMS_METunclustered_13TeV'))
+    
+    channel_labels = {'mujets': r'$\mu + \mathrm{jets}$', 'ejets': r'$e + \mathrm{jets}$'}
+    
+    
+    src_file = ROOT.TFile(args.input)
+    out_file = ROOT.TFile(args.output, 'recreate')
+    out_dirs = []
+    channels = []
+    
+    templates_to_keep = []
+    
+    
+    # Copy to the output file nominal signal templates and templates for
+    # systematic variations that do not need to be smoothed
+    for dir_key in src_file.GetListOfKeys():
+        if dir_key.GetClassName() != 'TDirectoryFile':
+            continue
+        
+        src_dir = dir_key.ReadObj()
+        out_dir = out_file.mkdir(src_dir.GetName())
+        channels.append(src_dir.GetName())
+        
+        for template_key in src_dir.GetListOfKeys():
+            name = template_key.GetName()
+            
+            if not sgn_name_regex.match(name):
+                continue
+            
+            smoothing_needed = False
+            
+            for _, syst in syst_names:
+                if name.endswith('_{}Up'.format(syst)) or name.endswith('_{}Down'.format(syst)):
+                    smoothing_needed = True
+                    break
+            
+            if smoothing_needed:
+                continue
+            
+            template = template_key.ReadObj()
+            template.SetDirectory(out_dir)
+            templates_to_keep.append(template)
+        
+        out_dirs.append(out_dir)
+    
+    
+    # Proceed to smoothing of requested variations
+    reader = Reader(src_file, channels, example=name)
+    templates_nominal = {t: reader.read_counts(sgn_template.format(t)) for t in sgn_types}
+    
+    for syst_display_name, syst_read_name in syst_names:
+        print 'smoothing', syst_display_name
+        templates_syst = {}
+        templates_syst['up'] = {
+            t: reader.read_counts('{}_{}Up'.format(sgn_template.format(t), syst_read_name))
+            for t in sgn_types
+        }
+        templates_syst['down'] = {
+            t: reader.read_counts('{}_{}Down'.format(sgn_template.format(t), syst_read_name))
+            for t in sgn_types
+        }
+        
+        for sgn_type, sgn_type_display in sgn_types.items():
+            # Construct smoothed variations
+            templates_smooth = {}
+            smoother = Smoother(
+                templates_nominal[sgn_type],
+                templates_syst['up'][sgn_type], templates_syst['down'][sgn_type],
+                DummyRebinner(),
+                nominalRebinned=templates_nominal[sgn_type],
+                upRebinned=templates_syst['up'][sgn_type],
+                downRebinned=templates_syst['down'][sgn_type]
+            )
+            templates_smooth['up'], templates_smooth['down'] = smoother.smooth(
+                (args.h_angle * reader.num_bins_angle, args.h_mass * reader.num_bins_mass)
+            )
+            
+            # Add smoothed variations to output directory
+            for ichannel, direction in itertools.product(range(len(channels)), ['up', 'down']):
+                unrolled_template = np.reshape(templates_smooth[direction][ichannel], (-1, 2))
+                hist = ROOT.TH1D(
+                    '{}_{}{}'.format(
+                        sgn_template.format(sgn_type), syst_read_name, direction.capitalize()
+                    ),
+                    '', reader.num_bins_angle * reader.num_bins_mass,
+                    0., reader.num_bins_angle * reader.num_bins_mass
+                )
+                
+                for i in range(len(unrolled_template)):
+                    hist.SetBinContent(i + 1, unrolled_template[i, 0])
+                    hist.SetBinError(i + 1, math.sqrt(unrolled_template[i, 1]))
+                
+                hist.SetDirectory(out_dirs[ichannel])
+                templates_to_keep.append(hist)
+            
+            if not args.fig_dir: continue
+            # Plot deviations
+            for ichannel, direction in itertools.product(range(len(channels)), ['up', 'down']):
+                input_deviation = (templates_syst[direction][sgn_type][ichannel, ..., 0]+10**-7) / \
+                    (templates_nominal[sgn_type][ichannel, ..., 0]+10**-7) - 1
+                smooth_deviation = (templates_smooth[direction][ichannel, ..., 0]+10**-7) / \
+                    (templates_nominal[sgn_type][ichannel, ..., 0]+10**-7) - 1
+
+                input_deviation = np.reshape(input_deviation, -1)
+                #clip and set to zero
+                input_deviation[input_deviation > 10**5] = 0
+                smooth_deviation = np.reshape(smooth_deviation, -1)
+                
+                chi2 = np.sum(
+                    (
+                        templates_smooth[direction][ichannel, ..., 0] - \
+                        templates_syst[direction][sgn_type][ichannel, ..., 0]
+                    ) ** 2 / templates_nominal[sgn_type][ichannel, ..., 1]
+                )
+                
+                
+                fig = plt.figure()
+                fig.patch.set_alpha(0.)
+                axes = fig.add_subplot(111)
+                
+                x = np.array(range(len(input_deviation) + 1))
+                axes.step(
+                    x, list(input_deviation) + [0], where='post',
+                    solid_joinstyle='miter', color='black', label='Original'
+                )
+                axes.step(
+                    x, list(smooth_deviation) + [0], where='post',
+                    solid_joinstyle='miter', color='r', label='Smoothed'
+                )
+                
+                axes.axhline(0., color='gray', lw=0.8, ls='dashed')
+                
+                for i in range(reader.num_bins_angle - 1):
+                    axes.axvline(
+                        len(input_deviation) / reader.num_bins_angle * (i + 1),
+                        color='gray', lw=0.8, ls='dashed'
+                    )
+                
+                axes.margins(x=0.)
+                axes.legend()
+                
+                axes.set_xlabel(
+                    r'$m_{t\bar t} \otimes \cos\/\theta^*_{\mathrm{t}_\ell}$ bin index'
+                )
+                axes.set_ylabel('Relative deviation from nominal')
+                axes.text(
+                    0., 1.003,
+                    '{}, {} ({}), {}, $h = ({:g}, {:g})$, $\\chi^2 = {:.2f}$'.format(
+                        sgn_type_display, syst_display_name, direction,
+                        channel_labels[channels[ichannel]], args.h_angle, args.h_mass,
+                        chi2
+                    ),
+                    ha='left', va='bottom', transform=axes.transAxes
+                )
+                
+                fig.savefig(os.path.join(
+                    args.fig_dir, '{}_{}_{}_{}.pdf'.format(
+                        sgn_type, syst_display_name, channels[ichannel], direction
+                    )
+                ))
+                plt.close(fig)
+    
+    
+    for out_dir in out_dirs:
+        out_dir.Write()
+    
+    out_file.Close()
+    src_file.Close()
